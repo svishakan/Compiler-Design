d10fc8623d32e0acf25bcbd842e1d9cb709db5b2
Vishakan Subramanian <svishakan123@gmail.com>
2021-02-05 15:28:39 +0530

    Added Ex02

---
+++ Ex02/Sample.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int a, b;
+    char str[5] = "Hello";
+
+    printf(str);
+    
+    a = b + 100;
+
+    if (a > b){
+        printf("Greater");
+    }
+
+    //a is GREATER than b!
+    /* Multi-line 
+    comment */
+
+    return 0;
+}
